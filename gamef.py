7fb8115ac519f382bbb1e94fe11780267c6a218d
Marc BEAUBOIS <marc@MacBook-Pro-de-Marc.local>
2017-12-25 23:21:04 +0000

    avancee significative

--- gamef.py
+++ gamef.py
@@ -26,17 +26,22 @@ class Interface(Frame):
         self.master.title("L'incroyable aventure")
         self.master.geometry("800x600+50+50")
         self.pack(fill=BOTH)
-       
+ 
+ # initiation des parametres du jeu
+        world.load_tiles()
+        self.player = Player()
+        self.room = world.tile_exists(self.player.location_x, self.player.location_y)      
+        
+# design de l'interface 
         self.titre = Label(self, text ="Un jeu d'aventure basique mais incroyablement trépidant peut être même épileptogène")
-        self.message = Label(self, width =40, relief ='sunken', wraplength =250, justify='left', text="Oui c'est vraiment un jeu d'aventure basique mais il me faut une phrase plus longue.") 
+        self.message = Label(self, width =40, relief ='sunken', wraplength =250, justify='left', text=self.room.intro_text()) 
         self.info = Label(self, text="Options possibles :")
         self.question = Label(self, text ="Qu'allez vous faire maintenant ?")
         self.entr1 = Entry(self)
         
         self.bouton_quitter = Button(self, text="Quitter", command=self.quit)
-        self.bouton_cliquer = Button(self, text="Commencer", fg="red",
+        self.bouton_avancer = Button(self, text="Avancer", fg="red",
             command=self.cliquer)
-
         # création d'un widget 'Canvas' contenant une image bitmap :
         self.can1 = Canvas(self, width =320, height =200, bg ='white')
         self.photo = PhotoImage(file ='resources/torche.gif')
@@ -49,40 +54,44 @@ class Interface(Frame):
         self.question.grid(row=4, column =1)
         self.entr1.grid(row =4, column =2)
         self.bouton_quitter.grid(row =5, column =3, sticky =E)
-        self.bouton_cliquer.grid(row=5, column =1, sticky =W)
+        self.bouton_avancer.grid(row=5, column =1, sticky =W)
         self.can1.grid(row=2, column=3, columnspan=1, rowspan=3,
                sticky=W+N, padx=5, pady=5)
-
 
     def cliquer(self):
         """Il y a eu un clic sur le bouton.
         On change la valeur du label message."""
-        world.load_tiles()
-        player = Player()
-        room = world.tile_exists(player.location_x, player.location_y)
-        self.message["text"] = room.intro_text()
-#        self.message["text"] = "Vous avez cliqué {} fois.".format(self.nb_clic)
-        while player.is_alive() and not player.victory:
-            room = world.tile_exists(player.location_x, player.location_y)
-            room.modify_player(player)
+
+        while self.player.is_alive() and not self.player.victory:
+            self.room = world.tile_exists(self.player.location_x, self.player.location_y)
+            self.room.modify_player(self.player)
             # Check again since the room could have changed the player's state
-            if player.is_alive() and not player.victory:
+            if self.player.is_alive() and not self.player.victory:
                 self.question["text"] = "Alors humain, que faire maintenant ?\n"
-                available_actions = room.available_actions()
-# definition du séparateur
-                s="\n"
-# définition d'une liste de string seq avec pour premier item le texte déjà affiché
-                seq=[self.info["text"]]
-# boucle de recherche et affichage des actions disponibles à afficher
-                for action in available_actions:
-                    seq.append(action.name)
-                self.info["text"] = s.join(seq) + "\n" 
+##                self.message["text"] = self.room.intro_text()
+                self.info["text"] = self.question["text"] + self.setavalaibleactions(self.room)
+
 # TODO : ajouter un bouton Go validant la saisie pour remplacer la fonction input ci dessous
                     #action_input = input('Action: ')
                 #for action in available_actions:
              #      if action_input == action.hotkey:
              #          player.do_action(action, **action.kwargs)
                 break
+        return()
+
+    def setavalaibleactions(self, studiedroom):
+# définition d'une liste de string seq vide
+        seq=[]
+        available_actions = studiedroom.available_actions()
+# definition du séparateur
+        s="\n"
+# boucle de recherche et affichage des actions disponibles à afficher
+        for action in available_actions:
+                 seq.append(action.name)
+# decoupe le resultat de la procedure en intercalant des separateurs et finalement un retours chariot
+        retour = s.join(seq) + "\n" 
+        return (retour)
+
 
 
 #    def play():
